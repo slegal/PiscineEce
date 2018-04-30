b89095880b5415b3d3ad87c33f81c6635d7d2aa7
slegal <solenn.le.gal@hotmail.fr>
2018-04-30 17:21:38 +0200

    debut
    debut css

---
+++ Piscine/Accueil.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<title>Accueil</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="designaccueil.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="fichier.js"></script>
+</head>
+<body>
+
+<div class="tete">
+<header>
+<div class="logo">
+        <img src="ece_logo.jpg" alt="Logo de l'Ece" width="100" height="100" >
+		</div>
+<div class="navigation">
+<nav>
+<ul>
+ <li><a href="#">Accueil</a></li>
+  <li><a href="#">Vous</a></li>
+<li><a href="#">Mon réseau</a></li>
+  <li><a href="#">Mes notifications</a></li>
+  <li><a href="#">Emplois</a></li>
+ </ul> 
+ </nav>
+ </div>
+	 </header>
+</div>
+</body>
+</html> 
