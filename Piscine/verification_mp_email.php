fed0a9d140bc3c4b24c34d584dc99f3e892aa163
LAPTOP-ICA5P4PV\user <mathildeangles@hotmail.fr>
2018-05-02 15:36:47 +0200

    verif_changement de page

--- Piscine/verification_mp_email.php
+++ Piscine/verification_mp_email.php
@@ -20,17 +20,30 @@ $db_found=mysqli_select_db($db_handle,$database);
 
     if($db_found) {
 		
-        $sql = "SELECT Adresse_email, Mot_de_passe, Num_utilisateur FROM utilisateur ";
+        $sql = "SELECT* FROM utilisateur ";
         $result = mysqli_query($db_handle, $sql) or die(mysql_error())  ;
         while($data = mysqli_fetch_assoc($result)) {
 	
 		//avec des variables session on recupere les info de la personne connectee
 		if(($data['Adresse_email']==$email) && ($data['Mot_de_passe']==$mp))
 		{
-			$num_u=$data['Num_utilisateur'];
+			$_SESSION['Num_utilisateur']=$data['Num_utilisateur'];
+			$_SESSION['Nom']=$data['Nom'];
+			$_SESSION['Prenom']=$data['Prenom'];
+			$_SESSION['Mot_de_passe']=$data['Mot_de_passe'];
+			$_SESSION['Adresse_email']=$data['Adresse_email'];
+			$_SESSION['Pseudo']=$data['Pseudo'];
+			$_SESSION['Lien_photo_profil']=$data['Lien_photo_profil'];
+			$_SESSION['Lien_photo_couverture']=$data['Lien_photo_couverture'];
+			$_SESSION['Description']=$data['Description'];
+			$_SESSION['Phrase_d_intro']=$data['Phrase_d_intro'];
+            $_SESSION['Type_user']=$data['Type_user'];
 			
-		// fonction qui recupere les experiences	avec en parametre $num_u////////////	
-		recupexp($num_u);
+			
+			header('Location: Vous.php');
+			
+			
+		
     }}}
     else { echo "Base de données non trouvée."; }
 
