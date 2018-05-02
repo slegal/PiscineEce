a64fbaa0a66a751c05f02e0c4c8d9391ecc4d339
VirgileDujet <38044199+VirgileDujet@users.noreply.github.com>
2018-05-02 16:25:27 +0200

    Recuperation et affichage experience

--- Piscine/fonction_recup-exp.php
+++ Piscine/fonction_recup-exp.php
@@ -9,23 +9,44 @@ function recupexp($num_u)
 	
     if($db_found) {
 		
-   ////// LE TABLEAU RETOURNE
-		$tabexpe = array();
+
 		
 		
-		$sql = "SELECT* FROM experience WHERE Num_utilisateur = $num_u ";
-        $result = mysqli_query($db_handle, $sql) or die(mysql_error())  ;
+		
+		$sql = "SELECT* FROM experience WHERE Num_utilisateur = $num_u "; ///REQUETE SQL
+		$result = mysqli_query($db_handle, $sql) or die(mysql_error())  ;
+		$p=0; ///VALEUR PERMETTANT D INCREMENTER LES LIGNES
 		
 		///REMPLISSAGE
 		foreach  ($db_handle->query($sql) as $row) {
-		$tabexpe[] = $row['Entreprise'];
-													}
-													
+			$tabexpe[$p][] = $row['Entreprise']; ///LIGNE A L INDICE $P , LA PREMIERE COLONNE = ENTREPRISE
+			$a ="A travaille dans l'entreprise "; ///$a EST LA CHAINE DE CARACTERE
+			$a .= $row['Entreprise']; ///ON CONCATENE
+			$tabexpe[$p][] = $row['Date_debut']; ///LIGNE A L INDICE $P , LA DEUXIEME COLONNE = DATE DEBUT
+			$a .=" depuis le ";
+			$a .= $row['Date_debut'];
+			$tabexpe[$p][] = $row['Date_fin'];
+			$a .=" jusqu'au ";
+			$a .= $row['Date_fin'];
+			$tabexpe[$p][] = $row['Poste'];
+			$a .=" au poste de ";
+			$a .= $row['Poste'];
+			
+			//echo $a;
+			$tabstringexpe[$p] = $a; ///TABLEAU AYANT AUTANT DE LIGNE ET CONTENANT LES LES PHRASES A AFFICHER
+			$p++; ///UNE FOIS LA LIGNE FINIE ON PASSE A LA LIGNE SUIVANTE EN INCREMENTANT
+			
+			
+			
+			}
+												
 		///AFFICHAGE
-		foreach($tabexpe as $element)
+		for ($g = 0; $g <sizeof($tabstringexpe);$g++)
 		{
-		echo $element . '<br />'; // affichera $prenoms[0], $prenoms[1] etc.
+		echo $tabstringexpe[$g];
+		?></br><?php
 		}
+		
 		}
     else { echo "Base de données non trouvée."; }
 
