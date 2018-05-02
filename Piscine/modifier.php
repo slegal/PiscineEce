98930bc45a5aa3d4e8d0a95bf1527a5cd1d6e64a
slegal <solenn.le.gal@hotmail.fr>
2018-05-02 12:33:18 +0200

    modifier

--- Piscine/modifier.php
+++ Piscine/modifier.php
@@ -18,49 +18,130 @@ echo 'num connecte '; echo $_SESSION['Lien_photo_profil'];
 <body>
 <h1>Modifier le profil</h1>
 <div class="introduction">
-<p> Vous pouvez modifier les informations de votre profil sur cette page. Le Nom, le Prénom et l'Email ne sont modifiable que par un administrateur de l'ECE.</p>
+<p> Vous pouvez modifier les informations de votre profil sur cette page. Le Nom, le Prénom et l'Email ne sont modifiable que par un administrateur de l'ECE. Vous
+pouvez également ajouter une expérience, une formatoin, une compétence et un centre d'interêt.</p>
 </div>
 
 <div class="container">
-  <form action="/action_page.php">
+  <form action="Vous.php">
     <div class="row">
       <div class="col-25">
         <label for="fname">Phrase d'introduction</label>
       </div>
       <div class="col-75">
-        <input type="text" id="introduction" name="firstname" value=<?php $_SESSION['Phrase_d_intro']?> >
+     <input type="text" id="introduction" name="introduction" value="<?php echo $_SESSION['Phrase_d_intro']?>" >
       </div>
     </div>
+   
     <div class="row">
       <div class="col-25">
-        <label for="lname">Last Name</label>
+        <label for="subject">Description</label>
       </div>
       <div class="col-75">
-        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+        <textarea id="descrpition" name="Description" value=""  style="height:200px"> <?php echo $_SESSION['Description']?></textarea>
       </div>
     </div>
+  
+	 <div class="row">
+	    <div class="col-25">
+	  <label for="input1">Experience  </label> 
+	  	<img src="plus.png" width="30" height="30" alt"plus">
+	  </div>
+	  <div class="col-60">
+       <input type="text" id="experience" value="entreprise1">
+	  </div>
+	<p>  Entreprise</p>
+ </div>
+     <div class="row">
+	<div class="col-25">
+	</div>
+       <div class="col-60">
+       <input type="text" id="poste" value="Poste occupe">
+   </div>
+   <p> Poste occupe</p>
+      </div>
     <div class="row">
-      <div class="col-25">
-        <label for="country">Country</label>
+	<div class="col-25">
+	</div>
+       <div class="col-60">
+       <input type="date" id="date debut" value="Date de debut">
+   </div>
+   <p> Date de début</p>
       </div>
-      <div class="col-75">
-        <select id="country" name="country">
-          <option value="australia">Australia</option>
-          <option value="canada">Canada</option>
-          <option value="usa">USA</option>
-        </select>
+	      <div class="row">
+	<div class="col-25">
+	</div>
+       <div class="col-60">
+       <input type="date" id="date fin" value="Date de fin">
+	   </div>
+	   <p> Date de fin</p>
+
+</div>
+
+
+	 <div class="row">
+	    <div class="col-25">
+	  <label for="input1">Formation </label>
+	  	  	<img src="plus.png" width="30" height="30" alt"plus">
+	  </div>
+	  <div class="col-60">
+       <input type="text" id="ecole" value="ecole ou université">
+	  </div>
+	<p>Ecole ou université</p>
+ </div>
+     <div class="row">
+	<div class="col-25">
+	</div>
+       <div class="col-60">
+       <input type="text" id="diplome" value="Diplome obtenu">
+   </div>
+   <p> Diplome obtenu</p>
       </div>
-    </div>
     <div class="row">
-      <div class="col-25">
-        <label for="subject">Subject</label>
+	<div class="col-25">
+	</div>
+       <div class="col-60">
+       <input type="text" id="description_formation" value="description formation">
+   </div>
+   <p> Description de la formation</p>
       </div>
-      <div class="col-75">
-        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-      </div>
-    </div>
-    <div class="row">
-      <input type="submit" value="Submit">
+	      <div class="row">
+	<div class="col-25">
+	</div>
+       <div class="col-60">
+       <input type="date" id="date_diplome" value="Date diplome">
+	   </div>
+	   <p> Date de l'obtention du diplome</p>
+</div>
+
+
+ <div class="row">
+	    <div class="col-25">
+	  <label for="input1">Compétences </label>
+	  	  	<img src="plus.png" width="30" height="30" alt"plus">
+	  </div>
+	  <div class="col-75">
+       <input type="text" id="competence" value="competence">
+	  </div>
+
+ </div>
+ 
+ 
+  <div class="row">
+	    <div class="col-25">
+	  <label for="input1">Centres d'interet </label>
+	  	  	<img src="plus.png" width="30" height="30" alt"plus">
+	  </div>
+	  <div class="col-75">
+       <input type="text" id="centre_interet" value="centre d'interet">
+	  </div>
+
+ </div>
+ 
+ 
+ 
+     <div class="row">
+      <input type="submit" value="Valider">
     </div>
   </form>
 </div>
