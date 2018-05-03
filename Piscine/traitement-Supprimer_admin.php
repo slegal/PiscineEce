92916b98c34de42e5156780b2c61254922216096
VirgileDujet <38044199+VirgileDujet@users.noreply.github.com>
2018-05-03 17:19:57 +0200

    reparation delete

--- Piscine/traitement-Supprimer_admin.php
+++ Piscine/traitement-Supprimer_admin.php
@@ -9,10 +9,15 @@ if($db_found) {
     
     $sql =  " DELETE FROM utilisateur WHERE Num_utilisateur = ".$_POST["personne"]."";
 
+    $sql2 =  " DELETE FROM contact WHERE Num_utilisateur = ".$_POST["personne"]."";
+
+    $sql3 =  " DELETE FROM contact WHERE Num_ami = ".$_POST["personne"]."";
     
    echo $sql;
     
     $result = mysqli_query($db_handle, $sql);  
+    $result = mysqli_query($db_handle, $sql2);  
+    $result = mysqli_query($db_handle, $sql3);  
     }
     
     else { echo "Base de données non trouvée."; }
